537c7c5ea338ae19b9833d2f44dccf78e5dcacf3
Okeref <117749157+Okeref@users.noreply.github.com>
2023-11-20 13:40:39 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -5,3 +5,4 @@ let  groupsdiv=document.querySelector(".groups");
 let url="http://api.football-data.org/v4/competitions/2000/standings";
 let token="33cae465963e4dcd86cce3fdd45ca561";
 axios.get(url, {
+    headers : {
